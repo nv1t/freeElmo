7ace4d1387e933d9e7a46887f16a08fa2cd9d440
nuit <nuit@hoeja.de>
2013-07-11 08:35:38 +0200

    source code cleanup #1

--- elmo.py
+++ elmo.py
@@ -6,29 +6,30 @@ import sys
 import time
 
 class Elmo:
-    device = None
-    msg = {
-        'version':   [0,0,0,0,0x18,0,0,0,0x10,0x8B,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        ,'picture':  [0,0,0,0,0x18,0,0,0,0x8e,0x80,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        ,'buttons':  [0,0,0,0,24,0,0,0,0,15,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    def __init__(self):
+        self.device = None
+        self.msg = {
+            'version':   [0,0,0,0,0x18,0,0,0,0x10,0x8B,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ,'picture':  [0,0,0,0,0x18,0,0,0,0x8e,0x80,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ,'buttons':  [0,0,0,0,24,0,0,0,0,15,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-        ,'zoom_stop':   [0,0,0,0,0x18,0,0,0,0xE0,0,0,0,0x00,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] #zoomin
-        ,'zoom_in':  [0,0,0,0,0x18,0,0,0,0xE0,0,0,0,0x01,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] #zoomout
-        ,'zoom_out': [0,0,0,0,0x18,0,0,0,0xE0,0,0,0,0x02,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        ,'autofocus':[0,0,0,0,0x18,0,0,0,0xE1,0,0,0,0x00,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] #autofocus?
-        ,'brightness_stop': [0,0,0,0,0x18,0,0,0,0xE2,0,0,0,0x04,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        ,'brightness_dark': [0,0,0,0,0x18,0,0,0,0xE2,0,0,0,0x03,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        ,'brightness_light': [0,0,0,0,0x18,0,0,0,0xE2,0,0,0,0x02,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        ,'brightness_auto': [0,0,0,0,0x18,0,0,0,0xE2,0,0,0,0x05,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        ,'focus_wide':  [0,0,0,0,0x18,0,0,0,0xEA,0,0,0,0x00,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
-        ,'focus_near':  [0,0,0,0,0x18,0,0,0,0xEA,0,0,0,0x01,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        ,'focus_stop':  [0,0,0,0,0x18,0,0,0,0xEA,0,0,0,0x02,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ,'zoom_stop':   [0,0,0,0,0x18,0,0,0,0xE0,0,0,0,0x00,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
+            ,'zoom_in':  [0,0,0,0,0x18,0,0,0,0xE0,0,0,0,0x01,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
+            ,'zoom_out': [0,0,0,0,0x18,0,0,0,0xE0,0,0,0,0x02,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ,'autofocus':[0,0,0,0,0x18,0,0,0,0xE1,0,0,0,0x00,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ,'brightness_stop': [0,0,0,0,0x18,0,0,0,0xE2,0,0,0,0x04,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ,'brightness_dark': [0,0,0,0,0x18,0,0,0,0xE2,0,0,0,0x03,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ,'brightness_light': [0,0,0,0,0x18,0,0,0,0xE2,0,0,0,0x02,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ,'brightness_auto': [0,0,0,0,0x18,0,0,0,0xE2,0,0,0,0x05,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ,'focus_wide':  [0,0,0,0,0x18,0,0,0,0xEA,0,0,0,0x00,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
+            ,'focus_near':  [0,0,0,0,0x18,0,0,0,0xEA,0,0,0,0x01,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ,'focus_stop':  [0,0,0,0,0x18,0,0,0,0xEA,0,0,0,0x02,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-    }
-    last_image = None #Image.open("error.png")
-    zooming = False
-    brightnessing = False
-    focusing = False
+        }
+        self.last_image = None
+        self.zooming = False
+        self.brightnessing = False
+        self.focusing = False
 
     def connect(self,vendor=0x09a1,product=0x001d):
         self.device = usb.core.find(idVendor=vendor, idProduct=product)
@@ -46,94 +47,70 @@ class Elmo:
 
     def zoom(self,i):
         if self.zooming:
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['zoom_stop'],0)
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
+            self.device.write(0x02,self.msg['zoom_stop'],0)
+            ret = self.device.read(0x81,32)
             self.zooming = False
             return
 
-        if i > 0:
-            self.zooming = True
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['zoom_in'],0)
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
-        elif i < 0:
-            self.zooming = True
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['zoom_out'],0)
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
-
+        if i > 0:   self.device.write(0x02,self.msg['zoom_in'],0)
+        elif i < 0: self.device.write(0x02,self.msg['zoom_out'],0)
+        self.zooming = True
+        ret = self.device.read(0x81,32)
 
     def focus(self,i):
         if self.focusing:                                                        
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['focus_stop'],0)
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
+            self.device.write(0x02,self.msg['focus_stop'],0)
+            ret = self.device.read(0x81,32)
             self.focusing = False                                                
             return                                                              
                                                                                 
-        if i > 0:                                                               
-            self.focusing = True                                                 
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['focus_wide'],0)
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
-        elif i < 0:                                                             
-            self.focusing = True                                                 
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['focus_near'],0)
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
+        if i > 0:   self.device.write(0x02,self.msg['focus_wide'],0)
+        elif i < 0: self.device.write(0x81,self.msg['focus_near'],0)
         else:
-            self.focusing = True                 
             self.autofocus()
+            return
+        self.focusing = True
+        ret = self.device.read(0x81,32)
         
 
 
     def brightness(self,i):
         if self.brightnessing:                                                        
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['brightness_stop'],0)
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
+            self.device.write(0x02,self.msg['brightness_stop'],0)
+            ret = self.device.read(0x81,32)
             self.brightnessing = False                                                
             return 
-        if i > 0:
-            self.brightnessing = True
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['brightness_light'],0)
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
-        elif i < 0:
-            self.brightnessing = True
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['brightness_dark'],0)
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
+
+        if i > 0:   self.device.write(0x02,self.msg['brightness_light'],0)
+        elif i < 0: self.device.write(0x02,self.msg['brightness_dark'],0)
         else:
-            self.brightnessing = True
             self.autobrightness()
+            return
+        self.brightnessing = True
+        ret = self.device.read(0x81,32)
 
     def autobrightness(self):
-        try:
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['brightness_auto'],0)
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
-        except:
-            pass
+        self.device.write(0x02,self.msg['brightness_auto'],0)
+        ret = self.device.read(0x81,32)
     
     def autofocus(self):
-        try:
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['autofocus'],0) 
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
-            return ret
-        except:
-            return False
+        self.device.write(0x02,self.msg['autofocus'],0) 
+        ret = self.device.read(0x81,32)
 
     def version(self):
-        try:
-            self.device.write(self.device[0][(0,0)][1].bEndpointAddress,self.msg['version'],0)
-            ret = self.device.read(self.device[0][(0,0)][0].bEndpointAddress,32)
-            return ret
-        except:
-            return False
+        self.device.write(0x02,self.msg['version'],0)
+        ret = self.device.read(0x81,32)
     
     def cleardevice(self):
-        '''Clear the devices memory'''
+        '''Clear the devices memory on endpoint 0x83'''
         while True:
             try:
                 results = self.device.read(0x83, 512)
             except usb.core.USBError as e:
                 if e.args[0] == 'Operation timed out' :
-                    break # timeout and swiped means we are done
+                    break # timeout means we are done
 
     def get_image(self):
-        # Try to read a picture until you get a valid stream
         try:
             self.device.write(0x04, self.msg['picture'],0)
             tmp = self.device.read(0x83,32)
@@ -143,32 +120,31 @@ class Elmo:
 
         # Every package has a defined length in byte 4/5 of the first 8 Bytes
         # which is used to read the whole package
-        finished = False                                                            
         error = False
-        answer = []                                                                 
-        while not finished:                                                         
+        answer = []  
+
+        # 0xfef8 is the maximum size of a package                           
+        # if it is smaller => the last package and exit                                                               
+        size = 0xfef8
+        while size == 0xfef8:                                                         
             try:
                 ret = self.device.read(0x83,512)                                               
-                size = 256*ret[5]+ret[4]#-(512-8) # Byte to integer 
-                answer += ret[8:]
-                answer += self.device.read(0x83,size)                                        
+                size = 256*ret[5]+ret[4]
+                answer += ret[8:]+self.device.read(0x83,size)                                        
             except:
                 self.cleardevice()
                 error = True
                 return self.last_image
-                break
-            # 0xfef8 is the maximum size of a package
-            # if it is smaller => the last package and exit
-            if not size == 0xfef8:                                                  
-                finished = True                                                     
 
         if not error:
             data = ''.join([chr(i) for i in answer])
             try:
                 stream = StringIO.StringIO(data)
                 image = Image.open(stream)
-                #pg_image = pygame.image.fromstring(image.tostring(), image.size, image.mode)
+
                 self.last_image = image
             except:
                 self.cleardevice()
             return self.last_image
+        else:
+            return False
